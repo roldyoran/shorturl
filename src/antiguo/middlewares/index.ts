cc675ff357c3b553f7d1e6ed2662adaf43a39df5
roldyoran <edgaralvares4208@gmail.com>
2025-06-07 18:19:59 -0600

    refactor: remove obsolete URL handling controllers, middlewares, routes, and utilities

--- src/antiguo/middlewares/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Middlewares para la aplicación de acortador de URLs
- * Exporta todos los middlewares desde sus archivos individuales
- */
-
-// Exportar todos los middlewares
-export { corsMiddleware } from './cors.middleware';
-export { urlServiceMiddleware } from './url-service.middleware';
-export { apiKeyMiddleware } from './api-key.middleware';
