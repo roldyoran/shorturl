f9b4dcd62cdf6b4b820f921e18e5ebdeca6996a8
roldyoran <edgaralvares4208@gmail.com>
2026-03-09 18:08:04 -0600

    feat: add frontend improvements and component updates

--- src/controllers/urls.zValidator.ts
+++
@@ -1,4 +0,0 @@
-import { zValidator } from "@hono/zod-validator";
-import { originalURLFormatSchema } from "@/models/zod.schemas";
-
-export const validateOriginalURL = zValidator("json", originalURLFormatSchema);
